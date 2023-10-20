c49b80dae18570614eb10c50612babfc804605f1
Jibon1209 <jessore007@gmail.com>
2023-10-20 14:11:23 +0600

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-//const uri = "mongodb+srv://<username>:<password>@cluster0.uiabrwm.mongodb.net/?retryWrites=true&w=majority";
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uiabrwm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -121,7 +121,7 @@ async function run() {
             const result = await cartCollection.insertOne(addToCare);
             res.send(result);
         });
-        
+
         app.delete('/carts/:id',async(req,res)=>{
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
